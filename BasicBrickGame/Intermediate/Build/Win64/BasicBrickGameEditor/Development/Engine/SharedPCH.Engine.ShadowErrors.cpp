97d61accd0c17eeac0c74ec4abf8963a78fff336
TheMOON <mert__balkan@hotmail.com>
2022-02-18 00:55:32 +0300

    Initial Commit

---
+++ BasicBrickGame/Intermediate/Build/Win64/BasicBrickGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29337
+#include "F:/GitRepos/BasicBrickGame/BasicBrickGame/Intermediate/Build/Win64/BasicBrickGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ BasicBrickGame/Source/BasicBrickGame/BasicBrickGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BasicBrickGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BasicBrickGame, "BasicBrickGame" );
